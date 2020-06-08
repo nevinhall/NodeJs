44690df0b19809720f57132e6b26420e959edb33
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-06-08 15:16:50 +0100

    create and implement partials using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
 
 const app = express()
-publicDirectoryPath = path.join(__dirname, '../public')
+
+//Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Set up handlebars view
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
+
 
 app.use(express.static(publicDirectoryPath))
 
+app.get('',(req,res) =>{
+    res.render('index',{
+        title: 'Still index',
+        name: 'created by nevin'
+
+    })
+
+})
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title: 'About Page',
+        name: 'Nevin'
+    })
+})
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        msg: 'This is the message b',
+        name: 'created by nevin'
+    })
+})
 
 
 app.get('/weather',(req,res)=>{
     res.send({
         forecast: 'sunny',
-        location: 'Cork'
+        location: 'Cork',
+        name: 'created by nevin'
     })
 })
 
