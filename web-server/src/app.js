c746132b74b61297db1894ea9c83ade2c17410f9
nevinhall <46722804+nevinhall@users.noreply.github.com>
2020-06-08 16:19:13 +0100

    add basic front end

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -19,7 +19,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res) =>{
     res.render('index',{
-        title: 'Still index',
+        title: 'Weather',
         name: 'created by nevin'
 
     })
